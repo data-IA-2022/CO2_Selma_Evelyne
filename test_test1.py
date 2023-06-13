922c814a87b644e18595ec38e76beca170faed3b
lysvallee <evelyne.theodose@gmail.com>
2023-06-13 16:33:18 +0200

    Update test_test1.py

--- test_test1.py
+++ test_test1.py
@@ -2,3 +2,6 @@ import pytest
 from os import environ
 from hello_app.views import recup_data
 
+def test_function_with_scenario_one():
+    print("Testing function with scenario one")
+    assert 1 + 1 == 2, f"Check addition value {1 + 1} does not match {2}"
