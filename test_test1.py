28b24cf68721d6199ed08c126681fd757b73f205
lysvallee <evelyne.theodose@gmail.com>
2023-06-13 16:16:36 +0200

    Update test_test1.py

--- test_test1.py
+++ test_test1.py
@@ -2,17 +2,3 @@ import pytest
 from os import environ
 from hello_app.views import recup_data
 
-def test_mock():
-  assert environ.get('BDD_URI')
-
-  try:
-    FileNotFoundError
-  except NameError:
-    FileNotFoundError = IOError
-
-  with pytest.raises(FileNotFoundError):
-    recup_data('')
-
-  recup_data('https://data.seattle.gov/api/views/2bpz-gwpy/rows.csv?accessType=DOWNLOAD')
-
-  assert True
